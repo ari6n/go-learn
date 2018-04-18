d1f78a54aa0c6237bf684a7791fc45a6efdff737
ogolovko <ogolovko@gmail.com>
2018-04-18 07:56:30 +0600

    Changed name for printSlice to PrintSlice to avoid redeclaration

--- making-slices.go
+++ making-slices.go
@@ -4,34 +4,34 @@ import "fmt"
 
 func main() {
 	a := make([]int, 5)
-	printSlice("a", a)
+	PrintSlice("a", a)
 
 	b := make([]int, 0, 5)
-	printSlice("b", b)
+	PrintSlice("b", b)
 
 	c := b[:2]
 	for i := range c {
 		c[i] = i
 	}
-	printSlice("c", c)
+	PrintSlice("c", c)
 
 	d := c[2:5]
-	printSlice("d", d)
+	PrintSlice("d", d)
 
 	c = c[:cap(c)]
-	printSlice("c", c)
+	PrintSlice("c", c)
 	for i := range c {
 		c[i] = i + 2
 	}
 
 	b = b[:cap(b)]
-	printSlice("b", b)
-	printSlice("c", c)
-	printSlice("d", d)
+	PrintSlice("b", b)
+	PrintSlice("c", c)
+	PrintSlice("d", d)
 
 }
 
-func printSlice(s string, x []int) {
+func PrintSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n",
 		s, len(x), cap(x), x)
 }
